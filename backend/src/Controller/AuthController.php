25daf08252c19e083c9c3c4b7c2183d5807a8092
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-11-05 09:52:35 +0100

    add student signup

--- backend/src/Controller/AuthController.php
+++ backend/src/Controller/AuthController.php
@@ -5,9 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AuthController extends AbstractController
 {
+
     #[Route('/auth', name: 'app_auth')]
     public function index(): Response
     {
@@ -15,4 +23,37 @@ class AuthController extends AbstractController
             'controller_name' => 'AuthController',
         ]);
     }
+
+    #[Route('/api/auth/student/signup', name: 'auth_student_signup', methods: ['POST'])]
+    public function auth_student_signup(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher
+    ): JsonResponse {
+
+        try {
+            $user = $serializer->deserialize($request->getContent(), Users::class, 'json');
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Invalid data format.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user->setStudent(true);
+        $user->setTeacher(false);
+
+        if ($user->getPassword()) {
+            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+        } else {
+            return new JsonResponse(['error' => 'Password is required.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);
+    }
+
+
 }
