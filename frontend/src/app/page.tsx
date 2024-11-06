4223f5f12c44a285953f952cde1815337fea8fa0
Arnaud R. <arno.royer2004@gmail.com>
2024-11-07 00:17:37 +0100

    add signin github button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import SignIn from "./components/sign-in";
 
 export default function Home() {
   return (
@@ -45,6 +46,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <SignIn />
       </main>
       <footer className={styles.footer}>
         <a
--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,18 +1,19 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-export const auth = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  // pages: {
-  // TODO   configurer la page de callback en cas de réussir
-  // TODO   configurer la page d'erreur en cas d'erreur
-  // }
-});
 
-export const { handlers, signIn, signOut } = auth;
+  callbacks: {
+    async redirect() {
+      return "http://localhost:4200";  // Redirection vers le bon port en dur
+      // TODO Trouver comment rediriger vers le bon port automatiquement, pas en dur
+    },
+  }
+});
