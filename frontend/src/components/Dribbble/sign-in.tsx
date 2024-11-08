dae629dcfc6fedf5eddacdf9dbf8d55c993e430a
Arnaud R <arno.royer2004@gmail.com>
2024-11-08 15:14:35 +0100

    wip github api

--- frontend/src/components/Dribbble/sign-in.tsx
+++ frontend/src/components/Dribbble/sign-in.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import { useSession }     from "next-auth/react";
 import { signInDribbble } from "@/actions";
 
 export default function SignIn() {
-  const { data: session } = useSession();
 
   return (
     <div>
-      {session ? (
-        <h1>Connecté en tant que {session.user?.name}</h1>
-      ) : (
-        <form action={signInDribbble}>
-          <button type="submit">Signin with Dribbble</button>
-        </form>
-      )}
+      <form action={signInDribbble}>
+        <button type="submit">Signin with Dribbble</button>
+      </form>
     </div>
   );
 }
