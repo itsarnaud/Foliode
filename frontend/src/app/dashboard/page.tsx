e545a74b203070ac8753accd5d567f779333d56a
Skiioo <merletti.sacha@gmail.com>
2024-12-10 16:36:40 +0100

    Add loader for 2seconds

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import Loader from '../../components/Loader/Loader';
+
 export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simuler le chargement des données
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 secondes
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <>
       <p>Dashboard</p>
--- frontend/src/app/dashboard/projects/page.tsx
+++ frontend/src/app/dashboard/projects/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import Loader from '../../../components/Loader/Loader';
+
 export default function Projects() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simuler le chargement des données
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 secondes
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+
   return (
     <>
       <p>Projects</p>
     </>
   )
 }
+/*
+useEffect(() => {
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      // Remplacez cette ligne par votre appel API réel
+      const response = await fetch('/api/data');
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error('Erreur lors du chargement des données:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  fetchData();
+}, []);
+
+if (loading) {
+  return <Loader />;
+}*/
