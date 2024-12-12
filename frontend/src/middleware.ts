fe37b7663b95377184b5b703da44ba9bc8ae4b37
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-12-12 09:33:32 +0100

    correction middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,6 +4,10 @@ import {NextRequest, NextResponse} from 'next/server';
 export function middleware(request: NextRequest) {
     const authCookie = request.cookies.get('token_auth');
 
+    if (!authCookie) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
     return NextResponse.next();
 }
 
