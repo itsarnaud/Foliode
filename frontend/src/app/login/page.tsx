e2fa09640a152d6dbbfd6143b6394382ec34aa68
Arnaud R <arno.royer2004@gmail.com>
2024-12-12 09:49:20 +0100

    Fix auth.ts, add userinfo in jwt

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -35,11 +35,10 @@ export default function LoginPage() {
         }));
     };
 
-<<<<<<< HEAD
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
 		setError("");
-	
+
 		try {
 			const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth`,
 				{ "email": data.email, "password": data.password },
@@ -47,7 +46,7 @@ export default function LoginPage() {
 			);
 
 			console.log(response)
-	
+
 			if (response.data.token) {
 				document.cookie = `token_auth=${response.data.token}; path=/`;
         router.push("/dashboard");
@@ -63,7 +62,7 @@ export default function LoginPage() {
 				setError("Erreur inattendue");
 			}
 		}
-	
+
 		if (!error) {
 			setData({
 				email: "",
@@ -71,50 +70,15 @@ export default function LoginPage() {
 			});
 		}
 	};
-=======
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setError("");
->>>>>>> 59568a5 (add input link components)
 
-        try {
-            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth`,
-                { "email": data.email, "password": data.password },
-                { headers: { "Content-Type": "application/json" }}
-            );
-
-            if (response.data.token) {
-                document.cookie = `token_auth=${response.data.token}; path=/`;
-                router.push("/dashboard");
-            }
-        } catch (err) {
-            if (axios.isAxiosError(err)) {
-                if (err.response?.status === 401) {
-                    setError("Email ou mot de passe incorrect");
-                } else {
-                    setError("Une erreur est survenue lors de la connexion");
-                }
-            } else {
-                setError("Erreur inattendue");
-            }
-        }
-
-        if (!error) {
-            setData({
-                email: "",
-                password: ""
-            });
-        }
-    };
-
-    const styles = {
-        inputWrapper: [
-            "border-primary",
-            "data-[hover=true]:border-primary-100",
-            "group-data-[focus=true]:border-primary"
-        ],
-        clearButton: "text-primary"
-    }
+	const styles = {
+		inputWrapper: [
+			"border-primary",
+			"data-[hover=true]:border-primary-100",
+			"group-data-[focus=true]:border-primary"
+		],
+		clearButton: "text-primary"
+	}
 
     return (
         <>
@@ -127,7 +91,6 @@ export default function LoginPage() {
                         <h1 className="text-lg font-bold">Connectez vous sur Foliode !</h1>
                     </div>
 
-<<<<<<< HEAD
 							<form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 items-center md:items-start">
 								<Input isRequired isClearable name="email" type="email" value={data.email} onChange={handleInputChange} variant="bordered" label="Email" placeholder="john.doe@example.com" classNames={styles} />
 								<Input
@@ -156,36 +119,6 @@ export default function LoginPage() {
 								<Buttons text="Se connecter" style="default" type="submit" />
 								<span className="text-sm sm:text-base">Pas de compte ? <Link href="/signup" className="cursor-pointer text-[#3E3F92] hover:text-[#5b5dd8] hover:underline">Créez votre compte !</Link> </span>
 							</form>
-=======
-                    <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 items-center md:items-start">
-                        <Input isRequired isClearable name="email" type="email" value={data.email} onChange={handleInputChange} variant="bordered" label="Email" placeholder="john.doe@example.com" classNames={styles} />
-                        <Input
-                            isRequired
-                            label="Mot de passe"
-                            variant="bordered"
-                            placeholder="Votre mot de passe"
-                            name="password"
-                            value={data.password}
-                            onChange={handleInputChange}
-                            endContent={
-                                <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
-                                    {isVisible ? (
-                                        <FaEyeSlash className="text-2xl text-primary pointer-events-none" />
-                                    ) : (
-                                        <IoEyeSharp className="text-2xl text-primary pointer-events-none" />
-                                    )}
-                                </button>
-                            }
-                            type={isVisible ? "text" : "password"}
-                            classNames={styles}
-                        />
-                        {/* TODO: Bien faire les messages d'erreur : https://nextui.org/docs/components/input */}
-                        {error && <p className="text-red-500 text-sm">{error}</p>}
-                        <span className="text-sm sm:text-base">Mot de passe oublié ? <Link href="/" className="cursor-pointer text-[#3E3F92] hover:text-[#5b5dd8] hover:underline">Cliquez ici !</Link> </span>
-                        <Buttons text="Se connecter" style="large-button" type="submit" />
-                        <span className="text-sm sm:text-base">Pas de compte ? <Link href="/signup" className="cursor-pointer text-[#3E3F92] hover:text-[#5b5dd8] hover:underline">Créez votre compte !</Link> </span>
-                    </form>
->>>>>>> 59568a5 (add input link components)
 
                     <div className="flex gap-5 items-center w-full">
                         <hr className="border border-primary w-full" />
@@ -197,13 +130,8 @@ export default function LoginPage() {
                         <DribbbleAuth />
                         <GithubAuth />
                     </div>
-
                 </div>
-
             </div>
-
         </>
-
-
     );
 }
