8187d081be4b9d130ebc277e150ef4a50868794b
Arnaud R. <arno.royer2004@gmail.com>
2024-12-17 19:52:48 +0100

    Add Formation Fixture

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -18,5 +18,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
