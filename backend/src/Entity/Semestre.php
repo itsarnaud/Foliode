0b4cd9b1a64597174c172a2717807e497ca123bd
Arnaud R. <arno.royer2004@gmail.com>
2024-12-17 22:51:53 +0100

    Add semestre Entity & add $is_first_connection to user entity

---
+++ backend/src/Entity/Semestre.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SemestreRepository;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SemestreRepository::class)]
+class Semestre
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "uuid", unique: true)]
+    #[ORM\GeneratedValue(strategy: "CUSTOM")]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private ?string $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(targetEntity: Formation::class, inversedBy: 'promotions')]
+    private ?Formation $formation = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFormation(): ?Formation
+    {
+        return $this->formation;
+    }
+
+    public function setFormation(?Formation $formation): static
+    {
+        $this->formation = $formation;
+        return $this;
+    }
+
+}
