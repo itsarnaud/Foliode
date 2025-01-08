6156f2041669cdeeadf455df160fe9751697d67d
Raphaël <115149179+rboucheron@users.noreply.github.com>
2025-01-08 11:37:51 +0100

    correctif project links

--- frontend/src/utils/formatData.ts
+++ frontend/src/utils/formatData.ts
@@ -1,31 +1,44 @@
-import {Project} from "@/interfaces/Project";
+import { Project } from "@/interfaces/Project";
 
 export const formatProjectsData = (projects: Project[]) => {
-    const projectsFormData = new FormData()
+  const projectsFormData = new FormData();
 
-    projects.forEach((project, index) => {
-        projectsFormData.append(`projects[${index}][title]`, project.title)
-        projectsFormData.append(`projects[${index}][description]`, project.description)
-        project.links.forEach((link, linkIndex) => {
-            projectsFormData.append(`projects[${index}][links][${linkIndex}]`, link)
-        })
-        project.images.forEach((image, imageIndex) => {
-            projectsFormData.append(`projects[${index}][images][${imageIndex}]`, image)
-        })
-    })
+  projects.forEach((project, index) => {
+    projectsFormData.append(`projects[${index}][title]`, project.title);
+    projectsFormData.append(
+      `projects[${index}][description]`,
+      project.description
+    );
+    project.projectsLinks.forEach((link, linkIndex) => {
+      projectsFormData.append(
+        `projects[${index}][projectsLinks][${linkIndex}][url]`,
+        link.url
+      );
+      projectsFormData.append(
+        `projects[${index}][projectsLinks][${linkIndex}][name]`,
+        link.name
+      );
+    });
+    project.images.forEach((image, imageIndex) => {
+      projectsFormData.append(
+        `projects[${index}][images][${imageIndex}]`,
+        image
+      );
+    });
+  });
 
-    return projectsFormData
-}
+  return projectsFormData;
+};
 
 export const formatToolsData = (tools: any[]) => {
-    const toolsFormData = new FormData()
+  const toolsFormData = new FormData();
 
-    tools.forEach((tool, index) => {
-        toolsFormData.append(`tools[${index}][name]`, tool.name)
-        if (tool.image) {
-            toolsFormData.append(`tools[${index}][image]`, tool.image)
-        }
-    })
+  tools.forEach((tool, index) => {
+    toolsFormData.append(`tools[${index}][name]`, tool.name);
+    if (tool.image) {
+      toolsFormData.append(`tools[${index}][image]`, tool.image);
+    }
+  });
 
-    return toolsFormData
-}
+  return toolsFormData;
+};
