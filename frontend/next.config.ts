b7a9db7269c5d4b1f21e240c82b1d8352e51b78e
Arnaud R. <arno.royer2004@gmail.com>
2025-01-29 11:03:39 +0100

    Revert "WIP: Dynamics subdomains"
    This reverts commit e7f9bbace2feab50099daf63f5992c1a74de63d8.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -17,40 +17,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        has: [{ type: 'host', value: 'www.localhost' }],
-        destination: '/',
-      },
-      {
-        source: '/',
-        has: [{ type: 'host', value: 'app.localhost' }],
-        destination: '/app/dashboard',
-      },
-      {
-        source: '/login',
-        has: [{ type: 'host', value: 'app.localhost' }],
-        destination: '/app/login',
-      },
-      {
-        source: '/signup',
-        has: [{ type: 'host', value: 'app.localhost' }],
-        destination: '/app/signup',
-      },
-      {
-        source: '/_next/static/:path*',
-        has: [{ type: 'host', value: 'app.localhost' }],
-        destination: '/_next/static/:path*',
-      },
-      {
-        source: '/foliode-icon.svg',
-        has: [{ type: 'host', value: 'app.localhost' }],
-        destination: '/foliode-icon.svg',
-      },
-    ]
-  }
 };
 
 export default nextConfig;
