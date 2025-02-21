3dea370a19f40f930cb38eaa05cf1c8169f675a6
Skiioo <merletti.sacha@gmail.com>
2025-02-21 16:26:14 +0100

    feat : intégration model 3d, correction

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,13 +4,13 @@ import { getDecodedToken }           from "./utils/serverJwtUtils";
 export function middleware(request: NextRequest) {
   const authCookie = getDecodedToken(request);
 
-  // if (!authCookie) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
+   if (!authCookie) {
+     return NextResponse.redirect(new URL("/login", request.url));
+   }
 
-  // if (authCookie.exp * 1000 < Date.now()) {
-  //   return NextResponse.redirect(new URL("/login", request.url));
-  // }
+   if (authCookie.exp * 1000 < Date.now()) {
+     return NextResponse.redirect(new URL("/login", request.url));
+   }
 
   return NextResponse.next();
 }
